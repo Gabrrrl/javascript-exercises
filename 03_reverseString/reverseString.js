b6281afa880f4de227e754ccf56fad4d9364bfb9
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-05 16:48:35 -0700

    Completed third javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let takeApartIntoArray = string.split('');
+    let reverseArray = takeApartIntoArray.reverse();
+    let joinTheArray = reverseArray.join('')
+    return joinTheArray
 };
+
+reverseString('hello there')
 
 // Do not edit below this line
 module.exports = reverseString;
