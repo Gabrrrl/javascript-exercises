84319c52acc46f143265b628bf9b845c1583374b
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-06 01:11:37 -0700

    Completed fifth javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    } else if (isNaN(a) || isNaN(b) || typeof (a) !== 'number' || typeof (b) !== 'number') {
+        return 'ERROR'
+    } else {
+        const high = Math.max(a, b);
+        const low = Math.min(a, b);
+        return high * (high + 1) / 2 - (low - 1) * (low) / 2;
+    }
 };
 
 // Do not edit below this line
