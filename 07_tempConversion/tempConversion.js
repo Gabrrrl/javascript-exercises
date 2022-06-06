466667c105ccbc8105cf039aab930fae757e40e6
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-06 02:21:46 -0700

    Completed seventh javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+// (f-32) * 5/9 = c
+const ftoc = function(fTemp) {
+  let temp = (fTemp - 32) * 5 / 9
+  let roundDown = Math.round(temp * 10) / 10
+ return roundDown
 };
-
-const ctof = function() {
-
+// (c*9/5) + 32 = f
+const ctof = function(cTemp) {
+  let temp = (cTemp * 9 / 5) + 32
+  let roundDown = Math.round(temp * 10) / 10
+return roundDown
 };
 
 // Do not edit below this line
