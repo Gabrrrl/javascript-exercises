361e769c03f27b566cf676562aba42903345a6ad
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-09 19:03:32 -0700

    Completed eighth javascript exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sumAll = nums.reduce((num1, num2) => num1 + num2, 0)
+	return sumAll
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+let multiplyAll = nums.reduce((num1, num2) => num1 * num2)
+  return multiplyAll
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1
+  if (num == 0 || num == 1) {
+    return answer
+  } else {
+    for (let i = num; i >= 1; i--) {
+      answer = answer * i
+    }
+    return answer
+  }
 };
 
 // Do not edit below this line
