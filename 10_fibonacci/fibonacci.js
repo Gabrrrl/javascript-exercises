e594c4dd808e0995e7c0615f2363ccd136ffb380
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-09 22:00:22 -0700

    Completed tenth javascript exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibonacciIndex) {
+    if (fibonacciIndex < 0) {
+        return 'OOPS'
+    }
+    let firstNum = 0;
+    let secondNum = 1;
+    for (let i = 1; i <= fibonacciIndex; i++) {
+        sequence = firstNum + secondNum;
+        firstNum = secondNum
+        secondNum = sequence
+    }
+    return firstNum
 };
 
 // Do not edit below this line
