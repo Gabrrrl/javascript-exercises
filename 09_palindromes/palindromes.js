82292dd2e7ca211804867f857f40c0b1480f26cc
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-09 21:37:04 -0700

    Completed ninth javascript exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    regExpr = /[^A-Za-z0-9]/g;
+    word = word.toLowerCase().replace(regExpr, '');
+    for (let i = 0; i < (word.length / 2); i++) {
+        if (word[i] !== word[word.length -1 - i]) {
+            return false
+        }
+    }
+        return true
 };
 
 // Do not edit below this line
