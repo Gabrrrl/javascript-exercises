202e5318df43ee83d3e4250bb43a6c6862f1ae4d
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-10 00:06:28 -0700

    Completed twelfth javascript exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (peopleArray) {
+    const thisYear = new Date().getFullYear();
+    return peopleArray.reduce((firstPerson, person) => {
+        let age = (person.yearOfDeath || thisYear) - person.yearOfBirth;        
+        return age > firstPerson.age ? { person, age } : firstPerson;
+    }, {person: null, age: 0 }).person;
+    };
+    
 // Do not edit below this line
 module.exports = findTheOldest;
